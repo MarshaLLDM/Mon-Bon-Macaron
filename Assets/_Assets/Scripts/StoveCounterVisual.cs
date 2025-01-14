043062f70087ea9bc11b28b78e1e4706c4509eb5
MarshaLLDM <pe4enko.pe4@yandex.ru>
2025-01-15 02:06:20 +0300

    Изменение 3D-модели миксера на мультиварку

--- Assets/_Assets/Scripts/StoveCounterVisual.cs
+++ Assets/_Assets/Scripts/StoveCounterVisual.cs
@@ -7,7 +7,6 @@ public class StoveCounterVisual : MonoBehaviour
     // Визуализация включения плиты и частиц
 
     [SerializeField] private StoveCounter _stoveCounter;
-    [SerializeField] private GameObject _stoveOnGameObject;
     [SerializeField] private GameObject _particlesGameObject;
 
     private void Start()
@@ -18,7 +17,6 @@ public class StoveCounterVisual : MonoBehaviour
     private void _stoveCounter_OnStateChanged(object sender, StoveCounter.OnStateChangedEventArgs e)
     {
         bool _showVisual = e._state == StoveCounter.State.Frying /*|| e._state == StoveCounter.State.Fried*/;
-        _stoveOnGameObject.SetActive(_showVisual);
         _particlesGameObject.SetActive(_showVisual);
     }
 }
