6c12f6212c57bea19b988da9d44a2a59caf69d19
MarshaLLDM <pe4enko.pe4@yandex.ru>
2025-01-15 02:27:01 +0300

    Revert "Изменение 3D-модели миксера на мультиварку"
    This reverts commit 043062f70087ea9bc11b28b78e1e4706c4509eb5.

--- Assets/_Assets/Scripts/StoveCounterVisual.cs
+++ Assets/_Assets/Scripts/StoveCounterVisual.cs
@@ -7,6 +7,7 @@ public class StoveCounterVisual : MonoBehaviour
     // Визуализация включения плиты и частиц
 
     [SerializeField] private StoveCounter _stoveCounter;
+    [SerializeField] private GameObject _stoveOnGameObject;
     [SerializeField] private GameObject _particlesGameObject;
 
     private void Start()
@@ -17,6 +18,7 @@ public class StoveCounterVisual : MonoBehaviour
     private void _stoveCounter_OnStateChanged(object sender, StoveCounter.OnStateChangedEventArgs e)
     {
         bool _showVisual = e._state == StoveCounter.State.Frying /*|| e._state == StoveCounter.State.Fried*/;
+        _stoveOnGameObject.SetActive(_showVisual);
         _particlesGameObject.SetActive(_showVisual);
     }
 }
